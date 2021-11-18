d8d4a3a80771f7a53bd352a7be889a2f20188a9a
Tae Hyung Kim <thkim1011@users.noreply.github.com>
2021-11-17 17:21:54 -0800

    Add background and header (#1)

---
+++ src/components/Row.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Box from "@mui/material/Box";
+
+interface RowProps {
+  isSelected: boolean;
+}
+const Row: React.FC<RowProps> = (props) => {
+  return (
+    <Box></Box>
+  );
+}
+
+export default Row;
